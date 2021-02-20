3e04bda6199c77ee70f159fec4ebb0bb83dc3c99
Stefan Lesser <mail@stefan-lesser.com>
2021-02-20 14:37:36 +0000

    Generalizing special forms.

--- Sources/TinyLisp/TinyLisp.swift
+++ Sources/TinyLisp/TinyLisp.swift
@@ -60,13 +60,14 @@ extension Expr {
             return context[atom.description] ?? self // lookup value or pass expression back unchanged
         case .list(let list):
             switch (list.first, list.dropFirst()) {
-            case (.none, _):                       return self // empty list (aka nil)
-            case (.atom("quote"), let arguments):  return arguments.first! // quote must not eval rest
-            case (.atom("if"), let arguments):     return try `if`(Array(arguments), &context)
-            case (.atom("label"), let arguments):  return try label(Array(arguments), &context)
-            case (.atom(let name), let arguments): return try apply(name.description, Array(arguments), &context) // function call
+            case (.none, _):
+                return self // empty list (aka nil)
+            case (.atom(let name), let arguments):
+                if let function = specialForms[name.description] { return try function(Array(arguments), &context) }
+                return try apply(name.description, Array(arguments), &context) // function call
             case (.list, _),
-                 (.function, _):                   throw InterpretationError(message: "First item in list not a symbol")
+                 (.function, _):
+                throw InterpretationError(message: "First item in list not a symbol")
             }
         case .function:
             throw InterpretationError(message: "Didn't expect a function here")
@@ -75,6 +76,43 @@ extension Expr {
 }
 
 extension Expr { // Special forms
+    var specialForms: [String: ([Expr], inout Environment) throws -> Expr] {
+        [
+            "quote": { arguments, _ in
+                return arguments.first! // quote must not eval rest
+            },
+            "if": { arguments, context in
+                guard
+                    let conditionExpr = arguments.first,
+                    let thenExpr =      arguments.dropFirst().first
+                else {
+                    throw InterpretationError(message: "'if': argument mismatch")
+                }
+                let elseExpr = arguments.dropFirst(2).first
+
+                let condition = try conditionExpr.eval(in: &context)
+                if case .list([]) = condition { // condition is false (empty list represents nil)
+                    guard let elseExpr = elseExpr else { return .list([]) } // else is optional
+                    return try elseExpr.eval(in: &context)
+                } else {
+                    return try thenExpr.eval(in: &context)
+                }
+            },
+            "label": { arguments, context in
+                guard
+                    case .atom(let name) =  arguments.first,
+                    let valueExpression =   arguments.dropFirst().first
+                else {
+                    throw InterpretationError(message: "'label': argument mismatch")
+                }
+
+                let value = try valueExpression.eval(in: &context)
+                context[name.description] = value
+                return value
+            }
+        ]
+    }
+
     private func lambda(_ list: [Expr], _ arguments: [Expr], _ context: Environment) throws -> Expr {
         guard
             case .atom("lambda")                = list.first,
@@ -92,37 +130,6 @@ extension Expr { // Special forms
             newContext[name.description] = value
         }
         return try functionBody.eval(in: &newContext)
-    }
-
-    private func `if`(_ rest: [Expr], _ context: inout Environment) throws -> Expr {
-        guard
-            let conditionExpr = rest.first,
-            let thenExpr =      rest.dropFirst().first
-        else {
-            throw InterpretationError(message: "'if': argument mismatch")
-        }
-        let elseExpr = rest.dropFirst(2).first
-
-        let condition = try conditionExpr.eval(in: &context)
-        if case .list([]) = condition { // condition is false (empty list represents nil)
-            guard let elseExpr = elseExpr else { return .list([]) } // else is optional
-            return try elseExpr.eval(in: &context)
-        } else {
-            return try thenExpr.eval(in: &context)
-        }
-    }
-
-    private func label(_ rest: [Expr], _ context: inout Environment) throws -> Expr {
-        guard
-            case .atom(let name) =  rest.first,
-            let valueExpression =   rest.dropFirst().first
-        else {
-            throw InterpretationError(message: "'label': argument mismatch")
-        }
-
-        let value = try valueExpression.eval(in: &context)
-        context[name.description] = value
-        return value
     }
 }
 
